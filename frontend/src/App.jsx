f5071d008643feb7adb48f43decdc39edb9ef272
Guillem Casas <gucasassi@gmail.com>
2023-11-24 18:35:44 +0100

    refactor(front): use root layout instead home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { Dashboard, Home, Login, Register } from "./pages";
+import RootLayout from "./components/layouts/RootLayout";
+import { Dashboard, Landing, Login, Register } from "./pages";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/register", element: <Register /> },
-  { path: "/login", element: <Login /> },
-  { path: "/dashboard", element: <Dashboard /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "dashboard", element: <Dashboard /> },
+    ],
+  },
 ]);
 
 const App = () => {
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,5 @@
 export { default as Dashboard } from "./Dashboard";
 export { default as Landing } from "./Landing";
-export { default as Home } from "./Home";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
 export { default as Error } from "./Error";
