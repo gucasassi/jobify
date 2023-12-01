51cef7e8cedaab1907aeea35a96addb667e259e6
Guillem Casas <gucasassi@gmail.com>
2023-12-01 12:12:20 +0100

    refactor(back): change job messages structures

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -2,33 +2,39 @@
 // @route   GET /api/v1/jobs
 // @access  Public
 export const getJobs = (req, res, next) => {
-  return res.status(200).json({ msg: "get all jobs" });
+  return res.status(200).json({ success: true, message: "get all jobs" });
 };
 
 // @desc    Get a single job
 // @route   GET /api/v1/jobs/:id
 // @access  Public
 export const getJob = (req, res, next) => {
-  return res.status(200).json({ msg: `get job with id ${req.params.id}` });
+  return res
+    .status(200)
+    .json({ success: true, message: `get job with id ${req.params.id}` });
 };
 
 // @desc    Get a single job
 // @route   POST /api/v1/jobs
 // @access  Private
 export const createJob = (req, res, next) => {
-  return res.status(200).json({ msg: "create a new job" });
+  return res.status(200).json({ success: true, message: "create a new job" });
 };
 
 // @desc    Get a single job
 // @route   POST /api/v1/jobs/:id
 // @access  Private
 export const updateJob = (req, res, next) => {
-  return res.status(200).json({ msg: `update job with id ${req.params.id}` });
+  return res
+    .status(200)
+    .json({ success: true, message: `update job with id ${req.params.id}` });
 };
 
 // @desc    Get a single job
 // @route   POST /api/v1/jobs/:id
 // @access  Private
 export const deleteJob = (req, res, next) => {
-  return res.status(200).json({ msg: `delete job with id ${req.params.id}` });
+  return res
+    .status(200)
+    .json({ success: true, message: `delete job with id ${req.params.id}` });
 };
