774b05923644207826955351fc0f2eed8c4b721a
Guillem Casas <gucasassi@gmail.com>
2023-12-17 18:22:07 +0100

    feat(jobs): add crud operations

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -1,40 +1,84 @@
+// Model
+import Job from "../models/Job.js";
+
 // @desc    Get all jobs
 // @route   GET /api/v1/jobs
 // @access  Public
-export const getJobs = (req, res, next) => {
-  return res.status(200).json({ success: true, message: "get all jobs" });
+export const getJobs = async (req, res, next) => {
+  const jobs = await Job.find();
+  return res
+    .status(200)
+    .json({ success: true, count: jobs.length, data: jobs });
 };
 
 // @desc    Get a single job
 // @route   GET /api/v1/jobs/:id
 // @access  Public
-export const getJob = (req, res, next) => {
-  return res
-    .status(200)
-    .json({ success: true, message: `get job with id ${req.params.id}` });
+export const getJob = async (req, res, next) => {
+  const { id } = req.params;
+  const job = await Job.findById(id);
+
+  // Validate Job
+  if (!job) {
+    return res
+      .status(404)
+      .json({ success: true, message: `Job with id ${id} does not exist` });
+  }
+
+  // Return success response
+  return res.status(200).json({ success: true, data: job });
 };
 
-// @desc    Get a single job
+// @desc    Create new job
 // @route   POST /api/v1/jobs
 // @access  Private
-export const createJob = (req, res, next) => {
-  return res.status(200).json({ success: true, message: "create a new job" });
+export const createJob = async (req, res, next) => {
+  const job = await Job.create(req.body);
+  return res.status(201).json({ success: true, data: job });
 };
 
-// @desc    Get a single job
-// @route   POST /api/v1/jobs/:id
+// @desc    Update existing job
+// @route   PUT /api/v1/jobs/:id
 // @access  Private
-export const updateJob = (req, res, next) => {
+export const updateJob = async (req, res, next) => {
+  // Update Job
+  const { id } = req.params;
+  const job = await Job.findOneAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  // Validate
+  if (!job) {
+    return res
+      .status(404)
+      .json({ success: false, message: `Job with id ${id} does not exist` });
+  }
+
+  // Return response
   return res
     .status(200)
     .json({ success: true, message: `update job with id ${req.params.id}` });
 };
 
-// @desc    Get a single job
-// @route   POST /api/v1/jobs/:id
+// @desc    Delete a job
+// @route   DELETE /api/v1/jobs/:id
 // @access  Private
-export const deleteJob = (req, res, next) => {
-  return res
-    .status(200)
-    .json({ success: true, message: `delete job with id ${req.params.id}` });
+export const deleteJob = async (req, res, next) => {
+  // Get Job
+  const { id } = req.params;
+  const job = await Job.findById(id);
+
+  // Validate
+  if (!job) {
+    return res
+      .status(404)
+      .json({ success: false, message: `Job with id ${id} does not exist` });
+  }
+
+  // Delete Job
+  await Job.deleteOne();
+
+  // Return response
+  return res.status(200).json({ success: true, data: {} });
 };
